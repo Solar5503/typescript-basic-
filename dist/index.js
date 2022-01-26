7473c326328207765433a3f166e43e2d548f5cd3
solar <spidervvv@mail.ru>
2022-01-26 23:35:43 +0300

    Add interfaces, classes and so on and so forth

--- dist/index.js
+++ dist/index.js
@@ -10,3 +10,69 @@ let ids = [1, 2, 3, 4, 5];
 let arr = [1, true, 'Hello'];
 //^ Tuple
 let person = [1, 'Vadim', true];
+//& Tuple array
+let employee;
+employee = [
+    [1, 'Brad'],
+    [2, 'Vadim'],
+    [3, 'Jill'],
+];
+//* Union
+let pid;
+pid = '22';
+//~ Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+const user = {
+    id: 1,
+    name: 'John',
+};
+//& Type assertion
+let cid = 1;
+// 1
+// let customerId = <number>cid;
+// 2
+let customerId = cid;
+//* Functions
+function addNum(x, y) {
+    return x + y;
+}
+// console.log(addNum(1, 5));
+function log(message) {
+    console.log(message);
+}
+const user1 = {
+    id: 1,
+    name: 'John',
+};
+const p1 = 1;
+const add = (x, y) => x + y;
+const sub = (x, y) => x - y;
+// console.log(sub(2, 3));
+//^ Classes
+class Person {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    register() {
+        return `${this.name} is now registered`;
+    }
+}
+const vadim = new Person(1, 'Vadim');
+// console.log(vadim);
+// vadim.id = 7;
+// console.log(vadim.id);
+console.log(vadim.register());
--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,117 @@ let arr: any[] = [1, true, 'Hello'];
 
 //^ Tuple
 let person: [number, string, boolean] = [1, 'Vadim', true];
+
+//& Tuple array
+let employee: [number, string][];
+employee = [
+  [1, 'Brad'],
+  [2, 'Vadim'],
+  [3, 'Jill'],
+];
+
+//* Union
+let pid: string | number;
+pid = '22';
+
+//~ Enum
+enum Direction1 {
+  Up = 1,
+  Down,
+  Left,
+  Right,
+}
+
+enum Direction2 {
+  Up = 'Up',
+  Down = 'Down',
+  Left = 'Left',
+  Right = 'Right',
+}
+// console.log(Direction1);
+
+//^ Objects
+// 1
+
+// const user: {
+//   id: number;
+//   name: string;
+// } = {
+//   id: 1,
+//   name: 'Vadim',
+// };
+
+//2
+
+type User = {
+  id: number;
+  name: string;
+};
+
+const user: User = {
+  id: 1,
+  name: 'John',
+};
+
+//& Type assertion
+let cid: any = 1;
+// 1
+// let customerId = <number>cid;
+
+// 2
+let customerId = cid as number;
+
+//* Functions
+function addNum(x: number, y: number): number {
+  return x + y;
+}
+// console.log(addNum(1, 5));
+
+function log(message: string | number): void {
+  console.log(message);
+}
+// log('1');
+
+//~ Interfaces
+interface UserInterface {
+  readonly id: number;
+  name: string;
+  age?: number;
+}
+
+const user1: UserInterface = {
+  id: 1,
+  name: 'John',
+};
+// console.log(user1);
+
+type Point = number | string;
+const p1: Point = 1;
+
+// user1.id = 5;
+
+interface MathFunc {
+  (x: number, y: number): number;
+}
+
+const add: MathFunc = (x: number, y: number): number => x + y;
+const sub: MathFunc = (x: number, y: number): number => x - y;
+// console.log(sub(2, 3));
+
+//^ Classes
+class Person {
+  private id: number;
+  name: string;
+  constructor(id: number, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+  register() {
+    return `${this.name} is now registered`;
+  }
+}
+const vadim = new Person(1, 'Vadim');
+// console.log(vadim);
+// vadim.id = 7;
+// console.log(vadim.id);
+console.log(vadim.register());

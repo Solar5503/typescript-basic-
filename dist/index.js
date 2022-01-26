3b2c8647e4ca1e719e99c8fa68d5b1b4b04a120b
Solar5503 <spidervvv@mail.ru>
2022-01-26 17:59:50 +0300

    add types

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,12 @@
 "use strict";
+//! Basic Types
 let id = 5;
-console.log('ID:', id);
+let company = 'Media';
+let isPublished = true;
+let x = 'Hello';
+let a;
+a = 30;
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, 'Hello'];
+//^ Tuple
+let person = [1, 'Vadim', true];
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+//! Basic Types
 let id: number = 5;
+let company: string = 'Media';
+let isPublished: boolean = true;
+let x: any = 'Hello';
+let a: number;
+a = 30;
 
-console.log('ID:', id);
+let ids: number[] = [1, 2, 3, 4, 5];
+let arr: any[] = [1, true, 'Hello'];
+
+//^ Tuple
+let person: [number, string, boolean] = [1, 'Vadim', true];
